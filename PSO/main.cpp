14858e8337c5576986e0dfbde23e6391cd443258
fabriond <gfbf@ic.ufal.br>
2020-01-26 22:34:49 -0300

    Improved PSO fitness function

--- PSO/main.cpp
+++ PSO/main.cpp
@@ -4,6 +4,7 @@
 #include <climits>
 #include <cmath>
 #include <ctime>
+#include <algorithm>
 
 int main(){
 
@@ -24,22 +25,50 @@ int main(){
     int swarmSize = 5;
     int dimensions = adjList.size()-1;
     std::vector<double> lb(dimensions, 0), ub(dimensions, nodeCount);
-    Function fitness(
-        [adjList, adjListCost](std::vector<double> pos) -> double {
-            std::set<int> visited;
-            int result = 0;
-            int oldPos = 0;
-            for(int i = 0; i < pos.size(); ++i){
-                if(!visited.insert(oldPos).second) return INT_MAX;
-                int val = (int)round(pos[i]) % adjList[oldPos].size();
-                int currentPos = adjList[oldPos][val];
-                result += adjListCost[oldPos][val];
-                oldPos = currentPos;
-            }
-            if(!visited.insert(oldPos).second) return INT_MAX;
-            return result;
+    int startPos = 0;
+    int bestStartPos = 0;
+    Function fitness;
+    fitness = [adjList, adjListCost, &startPos, &bestStartPos, &fitness](std::vector<double> pos) -> double {
+        if(startPos >= adjList.size()){
+            startPos = 0;
+            return INT_MAX;
         }
-    );
+        std::vector<int> visited(adjList.size());
+        int result = 0;
+        int oldPos = startPos;
+        for(int i = 0; i < pos.size(); ++i){
+            if(visited[oldPos] == 1){
+                //if this is a bad attempt, try with another starting position
+                startPos += 1;
+                return fitness.operator()(pos);
+            }
+            visited[oldPos] = 1;
+            int val = (int)round(pos[i]) % adjList[oldPos].size();
+            int currentPos = adjList[oldPos][val];
+            result += adjListCost[oldPos][val];
+            oldPos = currentPos;
+        }
+        if(visited[oldPos] == 1){
+            //if this is a bad attempt, try with another starting position
+            startPos += 1;
+            return fitness.operator()(pos);
+        }
+
+        std::vector<int>::const_iterator startPosIt = std::find(adjList[oldPos].begin(), adjList[oldPos].end(), startPos);
+        if(startPosIt != adjList[oldPos].end()){
+            //go back to the initial position, if there's a path leading there
+            int startPosLoc = std::distance(adjList[oldPos].begin(), startPosIt);
+            result += adjListCost[oldPos][startPosLoc];
+        } else {
+            //if we can't return to the start position from the last position, try with another starting position
+            startPos += 1;
+            return fitness.operator()(pos);
+        }
+
+        bestStartPos = startPos;
+        startPos = 0;
+        return result;
+    };
     
     //Running and benchmarking PSO
     clock_t t = clock();
@@ -51,13 +80,14 @@ int main(){
         std::cout << "Chosen Path: ";
         std::vector<int> resultArray;
         std::vector<double> pos = p.getPos();
-        int oldPos = 0;
+        int oldPos = bestStartPos;
         std::cout << oldPos;
         for(int i = 0; i < pos.size(); ++i){
             int currentPos = adjList[oldPos][(int)round(pos[i]) % adjList[oldPos].size()];
             oldPos = currentPos;
             std::cout << ", " << oldPos;
         }
+        std::cout << ", " << bestStartPos;
         std::cout << std::endl;
         std::cout << "Path's Cost: " << fitness(pos) << std::endl;
     } else{
